dc754ca9afa268144d310783c1cf3077f65fb273
Gustavo Alpendere <gustavo.alpendere@nextroll.com>
2020-08-23 22:41:43 -0300

    New game

---
+++ Assets/Game/UIBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class UIBehaviour : MonoBehaviour
+{
+    public TextMeshProUGUI score;
+    public TextMeshProUGUI time;
+
+    public GameState gameState;
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimeSpan timeSpan = TimeSpan.FromSeconds(gameState.time);
+        time.text = ((int)gameState.time).ToString();
+
+        score.text = gameState.score.ToString();
+    }
+}
